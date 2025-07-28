60806d1b06c52940f35a27f72b14ee114ac78332
laxman-goud <codewithlaxman92@gmail.com>
2025-07-28 19:05:55 +0530

    made responsive

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,8 @@ import { useState, useEffect } from "react";
 import NavBar from "./components/NavBar";
 import "./App.css";
 import { v4 as uuidv4 } from "uuid";
+import { FaEdit } from "react-icons/fa";
+import { MdDelete } from "react-icons/md";
 
 function App() {
   const [todo, setTodo] = useState("");
@@ -19,12 +21,13 @@ function App() {
   const toggleFinished = () => {
     setShowFinished(!showFinished)
   }
-  
+
   const addTodo = () => {
-    if(isEditMode){
+    if (isEditMode) {
       setEditMode(!isEditMode);
     }
     setTodos([...todos, { todo, id: uuidv4(), isCompleted: false }]);
+    setTodo('');
   };
 
   const editTodo = (id) => {
@@ -64,41 +67,42 @@ function App() {
   return (
     <>
       <NavBar />
-      <div className="container mx-auto my-5 rounded-xl p-5 bg-violet-100 min-h-[80vh]">
+      <div className="container mx-auto my-5 rounded-xl p-5 bg-violet-100 min-h-[80vh] max-w-[1000px]">
+        <h1 className="text-center font-bold text-[30px]">TickIt - Manage your todos at one place</h1>
         <div className="addTodo">
           <h2 className="text-3xl font-bold">Add a Todo</h2>
           <input
             onChange={handleChange}
-            onKeyDown={(e)=>{
-              if(e.key === 'Enter' && todo.length > 3){
+            onKeyDown={(e) => {
+              if (e.key === 'Enter' && todo.length > 3) {
                 addTodo();
               }
             }}
             type="text"
-            className="bg-white w-1/2 h-8 pl-2"
+            className="bg-white w-full h-9 pl-2 my-2.5 focus:border-blue-600 border-[1px] outline-none rounded-4xl"
             value={todo}
             placeholder="enter 3 minimum characters"
           />
           <button
-          disabled={todo.length < 3}
+            disabled={todo.length < 3}
             onClick={addTodo}
-            className="h-8 w-[6rem] bg-indigo-600 text-white px-5 rounded mx-4 hover:bg-indigo-900 cursor-pointer disabled:bg-indigo-400">
+            className="h-8 w-full bg-indigo-600 text-white px-5 mx-auto hover:bg-indigo-900 cursor-pointer disabled:bg-indigo-400 rounded-4xl">
             {isEditMode ? "Update" : "Add"}
           </button>
         </div>
-        <input type="checkbox" checked={showFinished} onChange={toggleFinished}/> Show Finished
+        <input type="checkbox" checked={showFinished} onChange={toggleFinished} className="my-4"/> Show Finished
+        <hr className="opacity-40 w-3/4 mx-auto my-2"/>
         <h2 className="text-2xl font-bold my-1.5">Your Todos</h2>
         {todos.length === 0 && <div className="mx-4">No Todos to Display</div>}
-        <div className="todos">
+        <div className="todos h-[54vh] overflow-y-scroll cursor-pointer pr-2">
           {todos.map((item) => {
-            return ( (showFinished || !item.isCompleted) && 
+            return ((showFinished || !item.isCompleted) &&
               <div
                 key={item.id}
                 className="todo flex justify-between bg-indigo-200 p-1.5 rounded my-1.5">
                 <div
-                  className={`text flex items-center gap-1.5 ${
-                    item.isCompleted ? "line-through" : ""
-                  }`}>
+                  className={`text flex items-center gap-3 ${item.isCompleted ? "line-through" : ""
+                    }`}>
                   <input
                     type="checkbox"
                     name={item.id}
@@ -107,20 +111,20 @@ function App() {
                   />
                   <h3>{item.todo}</h3>
                 </div>
-                <div className="buttons">
+                <div className="buttons flex items-center flex-wrap gap-1">
                   <button
                     onClick={() => {
                       editTodo(item.id);
                     }}
-                    className="h-8 w-[6rem] bg-indigo-600 text-white px-5 rounded mx-2 hover:bg-indigo-900 cursor-pointer">
-                    Edit
+                    className="h-8 bg-indigo-600 text-white px-5 rounded mx-2 hover:bg-indigo-900 cursor-pointer">
+                    <FaEdit />
                   </button>
                   <button
                     onClick={() => {
                       deleteTodo(item.id);
                     }}
-                    className="h-8 w-[6rem] bg-indigo-600 text-white px-5 rounded mx-2 hover:bg-indigo-900 cursor-pointer">
-                    Delete
+                    className="h-8 bg-indigo-600 text-white px-5 rounded mx-2 hover:bg-indigo-900 cursor-pointer">
+                    <MdDelete />
                   </button>
                 </div>
               </div>
